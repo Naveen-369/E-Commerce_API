006ec0cde889c863e7a0e6285492b7f1513842cd
Naveen Kumar <nk9363690@gmail.com>
2024-07-07 16:18:27 +0530

    MongoDB Connection for GET
    The Connection has been established for the GET methods which can get all the products to disaply as list and as well as get a produt by id

--- ApplicationProgrammingInterface/Controllers/ProductController.js
+++ ApplicationProgrammingInterface/Controllers/ProductController.js
@@ -1,15 +1,40 @@
-//Decalration of the callback function for the get method of the products in Routes
-exports.getProducts=(request,response,next)=>{
+const ProductModel = require("../Models/ProductModel");
+
+/*Decalration of the callback function for the get method of the products in Routes*/
+exports.getProducts=async(request,response,next)=>{
+    
+    //The database methods may take some time to fetch all the documents. So, we make the function asynchronous.
+    const ProductList=await ProductModel.find({});
+    
+    // Declaration of the response
     response.json({
         SuccessCode:true,
-        message:"Products fetched successfully"
+        message:"Products fetched successfully",
+        ProductList
     })
 }
 
-// Declaration of the callback function for the get method of the product id in Routes
-exports.getProductsID=(request,response,next)=>{
-    response.json({
-        SuccessCode:true,
-        message:"Products ID fetched successfully"
-    })
+/*Declaration of the callback function for the get method of the product id in Routes*/
+exports.getProductsID=async(request,response,next)=>{
+
+    // Fetching the product based on the id of the product.
+    try{
+        console.log(request.params.id);
+        const ProductID=await ProductModel.findById(request.params.id);
+        
+        //Declaration of the response
+        response.json({
+            SuccessCode:true,
+            message:"Products ID fetched successfully",
+            Product_Fetched:ProductID
+        })
+    }catch(error){
+
+            //Declaration of the response
+            response.json({
+                SuccessCode:false,
+                message:"Product ID not found Please enter a valid product ID",
+            })
+    }
+    
 }
---
+++ ApplicationProgrammingInterface/Models/ProductModel.js
@@ -0,0 +1,22 @@
+//Creating the mongoose instance
+const mongoose=require('mongoose')
+
+//Creating the schema for the product
+const ProductSchema=new mongoose.Schema({
+    name:String,
+    price:Number,
+    description:String,
+    ratings:String,
+    images:[{image:String}],
+    category:String,
+    Seller:String,
+    Stock:String,
+    Number_Of_Reviews:String,
+    CeratedAt:Date
+ })
+
+//Creating the model for the product. This model will be used to interact with the MongoDB database.
+const ProductModel= mongoose.model("Product",ProductSchema);
+
+//Exporting the model
+module.exports=ProductModel;
